ed60352f2ca5721d21136c876ff6314f6da983ed
ignanavas <dysrupterwow@gmail.com>
2022-10-30 18:52:24 -0300

    Hola mundo

--- BotonClear.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../stylesheets/BotonClear.css';
-const BotonClear = (props) => (
-  <div className='boton-clear'onClick={ props.manejarClear}>
-      {props.children}
-  </div>  
-);
-
-export default BotonClear;
--- componentes/Pantalla.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../stylesheets/Pantalla.css'
-const Pantalla =({ input }) => (
-    <div className='input'>
-        {input}
-    </div>
-);
-export default Pantalla;
